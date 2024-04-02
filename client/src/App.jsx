38ce054c1c5a754df1cfea8071ad5c7180f85e16
Igor <drapezaigor03@gmail.com>
2024-04-02 23:13:19 +0300

    added header and catch for empty search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './components/Header';
+
 import './css/Card.css';
+import './css/Search.css';
 
 function App() {
   const [details, setDetails] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -17,28 +21,42 @@ function App() {
     fetchDetails();
   }, []);
 
+  const filteredDetails = details.filter(detail =>
+    detail.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      details:
-      
-      {details.map(({ id, name, assemblyImg, disassemblyImg, description, components }) => (
-        <div key={id} className="detail-item">
-          <img className="detail-item__image" src={'./images/' + assemblyImg}
-          alt="loading image"/>
-        <div className="detail-info">
-          <div className="detail-list">
-            <p>Наименование: <span className="detail-list__name">{name}</span></p>
-            <p>Описание: <span className="detail-list__desc">{description}</span></p>
+      <Header />
+      <div className="detail-search">
+        <input
+          className='detail-search__input'
+          type="text"
+          placeholder="Поиск по названию детали"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      {filteredDetails.length === 0 ? (
+        <p>Данных не найдено</p>
+      ) : (
+        filteredDetails.map(({ id, name, assemblyImg, disassemblyImg, description, components }) => (
+          <div key={id} className="detail-item">
+            <img className="detail-item__image" src={'./images/' + assemblyImg} alt="loading image" />
+            <div className="detail-info">
+              <div className="detail-list">
+                <p>Наименование: <span className="detail-list__name">{name}</span></p>
+                <p>Описание: <span className="detail-list__desc">{description}</span></p>
+              </div>
+              <div className="detail-action">
+                <a className="detail-action__info" href="#">Подробнее</a>
+                <a className="detail-action__edit" href="#">Изменить</a>
+                <a className="detail-action__delete" href="#">Удалить</a>
+              </div>
+            </div>
           </div>
-          <div className="detail-action">
-            <a className="detail-action__info" href="#">Подробнее</a>
-            <a className="detail-action__edit" href="#">Изменить</a>
-            <a className="detail-action__delete" href="#">Удалить</a>
-          </div>
-        </div>
-        </div>
-      ))}
-
+        ))
+      )}
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import './../css/Header.css'
+
+function Header() {
+  return (
+    <header>
+      <p>Каталог деталей</p>
+    </header>
+  );
+}
+
+export default Header;
